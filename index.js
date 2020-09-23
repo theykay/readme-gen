cd0a45d2d3525411dd3b3022887bc79dc40d52d8
TheyKay <murraykyleb@gmail.com>
2020-09-22 21:54:52 -0600

    relocate mit license so name can update with input

--- index.js
+++ index.js
@@ -36,29 +36,28 @@ const badges = {
 
 let d = new Date();
 const year = d.getFullYear();
-let name;
 
-const mitLicense = `MIT License
+// const mitLicense = `MIT License
 
-Copyright (c) ${year} ${name}
+// Copyright (c) ${year} ${name}
 
-Permission is hereby granted, free of charge, to any person obtaining a copy
-of this software and associated documentation files (the "Software"), to deal
-in the Software without restriction, including without limitation the rights
-to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-copies of the Software, and to permit persons to whom the Software is
-furnished to do so, subject to the following conditions:
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
 
-The above copyright notice and this permission notice shall be included in all
-copies or substantial portions of the Software.
+// The above copyright notice and this permission notice shall be included in all
+// copies or substantial portions of the Software.
 
-THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
-SOFTWARE.`;
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+// SOFTWARE.`;
 
 
 
@@ -163,10 +162,10 @@ mkDirAsync("./package").catch((err) =>
         throw err;
     }
 });
+
 inquirer
 .prompt(questions)
 .then((answers) => {
-    name = answers.creator;
     multiline.install.push(answers.install);
     multiline.describe.push(answers.describe);
     multiline.use.push(answers.use);
@@ -174,6 +173,27 @@ inquirer
     let licenseBadge;
 
     if (answers.license === "MIT License") {
+        const mitLicense = `MIT License
+
+Copyright (c) ${year} ${answers.creator}
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.`;
         writeFileAsync("./package/license.txt", mitLicense).catch((err) => 
         {
             if (err) {
@@ -220,7 +240,6 @@ inquirer
 ${licenseBadge}
 
 ## Description
-![${answers.imageAlt}](${answers.image})
 ${answers.describe}
 
 ## Table of Contents
@@ -235,6 +254,7 @@ ${answers.describe}
 
 ## Usage
 \`${answers.use}\`
+![${answers.imageAlt}](${answers.image})
 
 ## License
 This application covered under the ${answers.license}
@@ -247,6 +267,7 @@ ${answers.contribution}
 
 ## answers
 Github: [${answers.github}](https://github.com/${answers.github})
+
 Email: [${answers.email}](mailto:${answers.email})
 `
     writeFileAsync("./package/readme.md", template).catch((err) => 
